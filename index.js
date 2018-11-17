4744d764a8b181d108082362f8d496e033281c41
Fei Qi <feiqi9047@gmail.com>
2018-11-17 20:25:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
-  for (var i = 0; string.length > i; i++){
+  for (var i = 0; array.length > i; i++){
   array.push(musicians[i] + " plays " + instruments[i])
   }
   return array
